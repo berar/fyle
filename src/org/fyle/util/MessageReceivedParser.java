817a6d4bb4c07b8f149823b20de5bdc1a0ab56fa
berar <aleksandar.berar@gmail.com>
2013-05-16 22:58:41 +0200

    remove all

--- src/org/fyle/util/MessageReceivedParser.java
+++
@@ -1,7 +0,0 @@
-package org.fyle.util;
-
-import org.fyle.util.mrhandler.MessageReceivedHandler;
-
-public interface MessageReceivedParser {
-	MessageReceivedHandler handle();
-}
--- src/org/fyle/util/mrhandler/MessageReceivedHandler.java
+++
@@ -1,7 +0,0 @@
-package org.fyle.util.mrhandler;
-
-import org.fyle.data.lr.LRInterface;
-
-public interface MessageReceivedHandler {
-	LRInterface createLRResponse();
-}
